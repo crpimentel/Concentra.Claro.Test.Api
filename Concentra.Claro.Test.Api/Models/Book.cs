d8fbc341a79ece6b08704a23b762ee412623b080
crpimentel <cristiantabarepimentel@gmail.com>
2024-10-24 18:59:02 -0400

    Subiendo Cambios para la estructura de folder del proyecto

---
+++ Concentra.Claro.Test.Api/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Concentra.Claro.Test.Api.Models
+{
+
+    // Models/Book.cs
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Esta es una aplicacion web api para una prueba de claro
